7abca8371180015c9a72e2e879a30e57b4dad93b
Mayur7030 <pawarmr9999@gmail.com>
2025-01-04 15:40:03 +0530

    Ready for hosting

---
+++ src/Utils/Reviews.js
@@ -0,0 +1,60 @@
+const Reviews = [
+  {
+    id: 1,
+    name: "Geetha sai",
+    bio: "Scrum Master, TCS",
+    img: "https://i.ibb.co/yPSV6Fv/dadang.jpg",
+    description:
+      "Dependable, innovative, and technically brilliant—Mayur embodies the qualities of an exceptional MERN stack developer.",
+    rating: 5.0,
+  },
+  {
+    id: 2,
+    name: "Amit Garg",
+    bio: "Senior Manager, Dassault systemes",
+    img: "https://i.ibb.co/chG1v0f/Amal.jpg",
+    description:
+      "Collaborating with Mayur has elevated our project quality. They are not just a developer but also an innovator, always bringing creative solutions to the table.",
+    rating: 5.0,
+  },
+  {
+    id: 3,
+    name: "Sai Krishna Chhava",
+    bio: "Software Developer",
+    img: "https://i.ibb.co/3M64m0f/aldi.jpg",
+    description:
+   "Working with Mayur is a pleasure; their React expertise drives projects forward, and their problem-solving skills are unmatched.",
+    rating: 5.0,
+  },
+  {
+    id: 4,
+    name: "Rohit Singh",
+    bio: "Full Stack Developer, Dassault Systemes",
+    img: "https://i.ibb.co/chG1v0f/Amal.jpg",
+    description:
+      "Mayur transforms challenging requirements into robust, scalable applications, inspiring others with dedication and quick learning.",
+    rating: 5.0,
+  },
+
+  {
+    id: 4,
+    name: "Sindhoory P V",
+    bio: "Senior QA, Dassault systemes",
+    img: "https://i.ibb.co/dKYnQM8/ilham.jpg",
+    description:
+      "What sets Mayur apart is their ability to understand complex requirements and deliver seamless, responsive designs that exceed expectations.",
+    rating: 5.0,
+  },
+
+  {
+    id: 4,
+    name: "Akash Pandey, TCS",
+    bio: "Full Stack Developer",
+    img: "https://i.ibb.co/chG1v0f/Amal.jpg",
+    description:
+      "The combination of technical expertise and a positive attitude makes Mayur an irreplaceable team member.",
+    rating: 5.0,
+  },
+];
+
+export default Reviews;
---
+++ src/components/molecules/Education/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Lottie from "react-lottie";
+import readingBook from "../../../assets/reading-book.json";
+
+const Education = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: readingBook,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div className="pt-24">
+      <div className="mb-12">
+        <h1 className="text-4xl font-semibold drop-shadow-md text-center">
+          My <span className="text-primary">Qualification</span>
+        </h1>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8">
+        <div className="">
+          <Lottie
+            options={defaultOptions}
+            height="70%"
+            width="90%"
+            className="mx-auto lg:mr-auto"
+          />
+        </div>
+        <div className="mx-auto lg:ml-auto">
+          {/* Diploma */}
+          <div className="p-6 bg-[#313131] bg-opacity-90 backdrop-blur-lg rounded-lg shadow-lg inline-block w-[300px] hover:shadow-primary duration-300 cursor-pointer">
+            <div className="text-right mb-4">
+              <h3 className="text-2xl font-semibold text-primary">
+Software Developer
+              </h3>
+              <p className="text-sm text-neutral font-semibold">2024-current</p>
+            </div>
+            <p className="text-sm text-neutral text-justify">
+            Currently, I am working as a Software Developer at Dassault Systèmes for the past year, contributing to their product "ENOVIA" and gaining extensive knowledge from scratch.
+            </p>
+          </div>
+
+          {/* React Developer */}
+          <div className="my-6 md:my-4 md:ml-[200px] p-6 bg-[#313131] bg-opacity-90 backdrop-blur-lg rounded-lg shadow-lg w-[300px] hover:shadow-primary duration-300 cursor-pointer">
+            <div className="text-right mb-4">
+              <h3 className="text-2xl font-semibold text-primary">
+                React Developer
+              </h3>
+              <p className="text-sm text-neutral font-semibold">2022-24</p>
+            </div>
+            <p className="text-sm text-neutral text-justify">
+            I worked as a React Developer at TCS, collaborating with GE, a USA-based client, for 2 years, delivering robust solutions.
+            </p>
+          </div>
+
+          {/* SSC */}
+          <div className="p-6 bg-[#313131] bg-opacity-90 backdrop-blur-lg rounded-lg shadow-lg w-[300px] hover:shadow-primary duration-300 cursor-pointer">
+            <div className="text-right mb-4">
+              <h3 className="text-2xl font-semibold text-primary">Educational Qualification</h3>
+              <p className="text-sm text-neutral font-semibold">2017-22</p>
+            </div>
+            <p className="text-sm text-neutral text-justify">
+            I hold a BE degree with an 8 CGPA and an MBA in Business Analytics with an 8.5 CGPA, both from Pune University.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Education;
